2a3b91a12caf1623b99ec82a97681446d93fc766
Sagar Dalal <73606213+sagarsec@users.noreply.github.com>
2024-11-16 00:15:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+set -x
 set +e
 usage() {
 	echo "Usage $0 [options] [ iso | ova | memstick | memstickserial | memstickadi | all | none ]"
